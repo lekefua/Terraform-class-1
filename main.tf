9f892f7bcaaba5cbb3cb32a083a3d91ac4d602b8
lekefua <145169940+lekefua@users.noreply.github.com>
2023-12-21 01:25:41 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 /*this is my first terraform code*/
-resource "azurerm_resource_group" "mcit420zz5um" {
+/*resource "azurerm_resource_group" "mcit420zz5um" {
   name     = "mcit_resource_lekefua-ZZ5-UM"
   location = "canadacentral"
 }
+*/
