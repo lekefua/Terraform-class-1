a208f09c7d969195a9b0f22a6c1e4068bfb1913c
lekefua <145169940+lekefua@users.noreply.github.com>
2024-01-11 02:29:32 +0100

    Update lookup.tf

--- lookup.tf
+++ lookup.tf
@@ -1,24 +1,12 @@
-variable "user_roles" {
-  type    = map(string)
-  default = {
-    alice = "admin"
-    bob   = "editor"
-    charlie = "viewer"
-  }
+variable "my_map"{
+type =map(string)
+default= {
+"key1"='value1"
+"key2"="value2"
 }
-variable "default_role" {
-  type    = string
-  default = "guest"
+locals{
+my_value=lookup(var.my_map,"key1", "")
 }
-
-locals {
-  user_role_lookup = {
-    alice   = lookup(var.user_roles, "alice", var.default_role)
-    bob     = lookup(var.user_roles, "bob", var.default_role)
-    david   = lookup(var.user_roles, "david", var.default_role)
-  }
-}
-
-output "user_roles_result" {
-  value = local.user_role_lookup
+output "my_value-output" {
+value = local.my_value
 }
